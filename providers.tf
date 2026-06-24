300760eabe6010b238f80e1c2dbb0c381dc25b1f
acer <acer@Kushs-MacBook-Air.local>
2026-06-24 19:06:10 -0400

    Added new changes

--- providers.tf
+++ providers.tf
@@ -8,6 +8,15 @@ terraform {
   }
 }
 
+resource "aws_ecr_repository" "app_repo" {
+  name                 = "devops-core-app"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
 provider "aws" {
   region = "us-east-1" # Deploying into Northern Virginia
 }
